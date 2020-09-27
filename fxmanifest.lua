a706aa5ecf6962169a727c397ecb1d36d2b1c338
gaLcH <48186229+GBGamingCommunity@users.noreply.github.com>
2020-09-27 15:20:12 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+--[[
+GBGC Server - connect gbgc.fm
+Discord - https://discord.gg/8Gzv44U
+]]
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Great British Gaming Community - GBGC'
+
+server_scripts {
+    '*.lua'
+}
